21136d0cd923b3bba38ecf65b54f392bad4f4950
2k5Quill <tranminhquang03112005@gmail.com>
2025-11-01 15:22:21 +0700

    delete unecessary comment

--- View/MajorView.h
+++ View/MajorView.h
@@ -1,28 +1,6 @@
  #ifndef MAJOR_VIEW_H
  #define MAJOR_VIEW_H
-
- // Forward declaration needed if Major struct is used, but it isn't directly needed here
- // #include "../Model/Major.h"
-
- /**
-  * @brief Displays the menu options for Major Management.
-  * @return The user's validated integer choice (0-4).
-  */
  int displayMajorMenu();
-
- /**
-  * @brief Displays the list of all majors in a formatted table (3 columns).
-  * Requires data fetched by controller functions (getStudentCountForMajor, getStudentIDsForMajor).
-  */
  void displayMajorList();
-
- /**
-  * @brief Handles the user interaction for option 3: Add/Remove Student from Major.
-  * Prompts for Major Code, action (add/remove), and Student ID.
-  */
  void handleUpdateMajorStudents();
-
- // Note: The main loop function runMajorManagementSession() is kept in main.c
- // following the pattern of runSubjectManagementSession().
-
  #endif // MAJOR_VIEW_H
